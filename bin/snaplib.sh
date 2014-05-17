ed1e569e75aadbbae68b8d0a0b9570fde9b18930
Scott Weikart <sweikart@gmail.com>
2014-05-16 19:31:11 -0700

    Added copyright notices

--- bin/snaplib.sh
+++ bin/snaplib.sh
@@ -1,5 +1,25 @@
 :
 
+#     snap is a utility to store some directories on a server, versioned.
+#
+# Copyright (C) 2014, Human Rights Data Analysis Group (HRDAG)
+#     https://hrdag.org
+#
+# This file is part of snap.
+#
+# Foobar is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Foobar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with snap.  If not, see <http://www.gnu.org/licenses/>.
+
 set -u					# error if expand unset variable
 
 umask 02				# we use group perms
