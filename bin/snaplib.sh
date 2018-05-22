c69d1965beaba29de8dfa41ce6ec75836cb89494
Scott Weikart <sweikart@gmail.com>
2018-05-22 13:15:21 -0700

    If push fails because of snapserv error (e.g. project doesn't exist), show the snapserv error message instead of the generic message that assumes rsync failed.

--- bin/snaplib.sh
+++ bin/snaplib.sh
@@ -20,7 +20,7 @@
 # You should have received a copy of the GNU General Public License
 # along with snap.  If not, see <http://www.gnu.org/licenses/>.
 
-Version=0.4.6		     # alert user if internal 'ssh' generates an error
+Version=0.4.7		     # alert user if internal 'ssh' generates an error
 Version_required=0.2.18	     # 'snap log' records had old revision not new one
 Version_required=0.2.19	     # when 'snap push', don't show 0B transfers
 Version_required=0.2.20	     # sort .snap/files-* just before we use them
@@ -34,6 +34,7 @@ Version_required=0.4.3	     # update copyright dates
 Version_required=0.4.4	     # explain why a bad tag name is illegal
 Version_required=0.4.5	     # alert user if internal 'ssh' generates an error
 Version_required=0.4.6	     # must sort files-* files *after* append metadata
+Version_required=0.4.7	     # if snapserv error msg, show it (not rsync msg)
 
 maintainer="Scott Weikart <sweikart@gmail.com>" # can over-ride in config file
 
