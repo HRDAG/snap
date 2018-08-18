2ae4198b2649c85d21dd2c6f1d5390eb3386f47a
Scott Weikart <sweikart@gmail.com>
2018-08-18 16:51:08 -0700

    separate out roles of (code) maintainer vs administrator

--- etc/snapserv.sh
+++ etc/snapserv.sh
@@ -1,1 +1,5 @@
-snapserv_root=				# location of repo
+snapserv_root=		# location of repo
+
+# maintainer=		# if Scott Weikart doesn't maintain your code
+
+# administrator=	# person who manages perms/etc (defaults to maintainer)
